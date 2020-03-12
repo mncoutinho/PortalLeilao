a85cd0bb9c30b369601d8e577c6790dce797fca3
Lucas Fernandes da Silva <47428909+Planctor@users.noreply.github.com>
2020-03-11 22:45:01 -0300

    Add files via upload
    SE PUDER ALGUEM CRIAR ROTAS

---
+++ client/src/pages/login.vue
@@ -0,0 +1,23 @@
+<template>
+    <v-container>
+               
+        <v-card 
+        color="pink"
+        width="256px"
+        height="400px">
+                <v-card-text>
+                   
+                </v-card-text>
+        </v-card>          
+        
+    </v-container>
+</template>
+
+
+<script>
+export default {
+  data: () => ({
+    drawer: null
+  })
+}
+</script>
