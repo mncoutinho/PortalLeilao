1e9f4a035c901f8bad184a64f4122b69219553d8
Lucas Fernandes da Silva <47428909+Planctor@users.noreply.github.com>
2020-03-12 22:57:18 -0300

    Add files via upload
    Dei um apgrade no login...
    Preciso q alguem formate/posicione os textos ...
    e é isso ai

--- client/src/pages/login.vue
+++ client/src/pages/login.vue
@@ -1,16 +1,66 @@
 <template>
-    <v-container>
-               
-        <v-card 
-        color="pink"
-        width="256px"
-        height="400px">
-                <v-card-text>
-                   
+    <v-content>
+      
+      <v-row>
+        <v-card class="mx-auto mb-12" :elevation="12">
+          <v-flex class="d-flex justify-center" >
+            <v-card 
+            class=""
+            max-width="400"
+            color="indigo"
+            height="450px"
+            width="400px"
+            
+            >
+              <v-row class="fill-height">
+               <v-card-title  class=".display-3 white--text text-center font-weight-medium" >
+                  
+                  SEJA BEM-VINDO !
+                    
+                </v-card-title>
+                <v-card-text class="text-primary white--text " sm="4">
+                  Todos estamos ansiosos por conhecer você melhor, tanto ao nível profissional como pessoal. Aqui somos como uma família e queremos que se sinta desde já parte dela. Não temos dúvidas que você trará muitas valências e que aprenderemos muito com você.
                 </v-card-text>
-        </v-card>          
+                </v-row>
+            </v-card>
+
+            <v-card
+            max-width="400"
+            color="white "
+            height="450px"
+            width="400px"
+            >
+             
+              <v-card-text>
+                <v-card-title>Login</v-card-title>
+                <v-form>
+                  <v-text-field
+                    label="E-mail"
+                    name="login"
+                    
+                    type="text"
+                  ></v-text-field>
+
+                  <v-text-field
+                    id="password"
+                    label="Senha"
+                    name="password"
         
-    </v-container>
+                    type="password"
+                  ></v-text-field>
+                </v-form>
+              </v-card-text>
+
+              <v-card-actions>
+                <v-spacer/>
+                <v-btn color="primary">Entrar</v-btn>
+              </v-card-actions>
+            </v-card>
+            
+          </v-flex>
+        </v-card>
+      </v-row>
+    </v-content>
 </template>
 
 
