a1015c1c0d25367e4704b8927ad7b119a8103fd6
Lucas Fernandes da Silva <47428909+Planctor@users.noreply.github.com>
2020-03-13 22:34:40 -0300

    Add files via upload

--- client/src/pages/criar.vue
+++ client/src/pages/criar.vue
@@ -1,151 +1,126 @@
 <template>
     <v-content>
         <v-row justify="center">
-            <v-col cols="12" sm="10" md="8" lg="6">
-                <v-card ref="form">
-                <v-card-text>
-                    <v-text-field
-                    ref="name"
-                    v-model="name"
-                    :rules="[() => !!name || 'This field is required']"
-                    :error-messages="errorMessages"
-                    label="Full Name"
-                    placeholder="John Doe"
-                    required
-                    ></v-text-field>
-                    <v-text-field
-                    ref="address"
-                    v-model="address"
-                    :rules="[
-                        () => !!address || 'This field is required',
-                        () => !!address && address.length <= 25 || 'Address must be less than 25 characters',
-                        addressCheck
-                    ]"
-                    label="Address Line"
-                    placeholder="Snowy Rock Pl"
-                    counter="25"
-                    required
-                    ></v-text-field>
-                    <v-text-field
-                    ref="city"
-                    v-model="city"
-                    :rules="[() => !!city || 'This field is required', addressCheck]"
-                    label="City"
-                    placeholder="El Paso"
-                    required
-                    ></v-text-field>
-                    <v-text-field
-                    ref="state"
-                    v-model="state"
-                    :rules="[() => !!state || 'This field is required']"
-                    label="State/Province/Region"
-                    required
-                    placeholder="TX"
-                    ></v-text-field>
-                    <v-text-field
-                    ref="zip"
-                    v-model="zip"
-                    :rules="[() => !!zip || 'This field is required']"
-                    label="ZIP / Postal Code"
-                    required
-                    placeholder="79938"
-                    ></v-text-field>
-                    <v-autocomplete
-                    ref="country"
-                    v-model="country"
-                    :rules="[() => !!country || 'This field is required']"
-                    :items="countries"
-                    label="Country"
-                    placeholder="Select..."
-                    required
-                    ></v-autocomplete>
-                </v-card-text>
-                <v-divider class="mt-12"></v-divider>
-                <v-card-actions>
-                    <v-btn text>Cancel</v-btn>
-                    <v-spacer></v-spacer>
-                    <v-slide-x-reverse-transition>
-                    <v-tooltip
-                        v-if="formHasErrors"
-                        left
+           <v-card color="grey" max-width="800" class="mb-12"  :elevation="10">
+               <v-flex class="d-flex ">
+                <v-card
+                width="250px"
+                height="500px"
+                color="blue"
+                 
+                >
+
+                </v-card >
+                    <v-card 
+                    width="450px"
+                    height="500px"
+                    color="white"
                     >
-                        <template v-slot:activator="{ on }">
-                        <v-btn
-                            icon
-                            class="my-0"
-                            @click="resetForm"
-                            v-on="on"
+                        <v-text-title
+                        class="blue--text "
                         >
-                            <v-icon>mdi-refresh</v-icon>
-                        </v-btn>
-                        </template>
-                        <span>Refresh form</span>
-                    </v-tooltip>
-                    </v-slide-x-reverse-transition>
-                    <v-btn color="primary" text @click="submit">ENVIAR</v-btn>
-                </v-card-actions>
-                </v-card>
-            </v-col>
+                            CRIE JÁ A SUA CONTA
+                        </v-text-title>
+                        <v-card-text>
+                            <!--NOME COMPLETO-->
+                            <v-text-field
+                                ref="nome"
+                                v-model="nome"
+                                :rules="[() => !!nome || 'Este campo é necessario']"
+                                :error-messages="errorMessages"
+                                label="Nome Completo"
+                                placeholder="Ricardo Milos dos Santos"
+                                required
+                                
+                            >
+                            </v-text-field>
+                            <!--EMAIL-->
+                            <v-text-field
+                                ref="e-mail"
+                                v-model="email"
+                                :rules="[
+                                    () => !!email || 'Este campo é necessario',
+                                    () => !!email && email.length <= 25 || 'Endereço de e-mail prescisa de no maximo 25 caracter',
+                                    addressCheck
+                                ]"
+                                label="E-mail"
+                                placeholder="Ricardo@Milos.com"
+                                counter="25"
+                                required
+                                >
+                            </v-text-field>
+                            <!--CPF-->
+                            <v-text-field
+                                ref="CPF"
+                                v-model="CPF"
+                                :rules="[() => !!CPF || 'Este campo é necessario',
+                                () => !!CPF && CPF.length === 11 || 'DIGITE 11 CARACTERES',
+                                addressCheck]"
+                                :error-messages="errorMessages"
+                                label="CPF"
+                                placeholder="123-456-789-10"
+                                required
+                            >
+                            </v-text-field>
+                            <!--SENHA-->    
+                            <v-text-field
+                                ref="nome"
+                                v-model="senha"
+                                :rules="[
+                                    () => !!senha || 'Este campo é necessario',
+                                    () => !!senha && senha.length >= 5 || 'Digite no minimo 5 caracter',
+                                    addressCheck
+                                ]"
+                                :error-messages="errorMessages"
+                                :type="'password'"
+                                label="Senha"
+                                placeholder="**********"
+                                required
+                            >
+                            </v-text-field>
+                            <!--COMFIRMAR SENHA-->
+                            <v-text-field
+                                ref="nome"
+                                v-model="senha"
+                                :rules="[
+                                    () => !!senha || 'Este campo é necessario',
+                                    () => !!senha && senha.length >= 5 || 'Digite no minimo 5 caracter',
+                                    addressCheck
+                                ]"
+                                :error-messages="errorMessages"
+                                :type="'password'"
+                                label="Comfirme a sua senha"
+                                placeholder="**********"
+                                required
+                            >
+                            </v-text-field>
+                            
+                                <v-checkbox
+                                label="aceitar os termos de acesso"
+                                >
+
+                                </v-checkbox>
+
+                            <v-flex class="d-flex ">
+                                <v-btn 
+                                 
+                                @click="resetForm"
+                                v-on="on"
+                                text>
+                                Cancelar
+                                </v-btn>
+                            <v-spacer/>
+                                <v-btn color="primary"  @click="submit">ENVIAR</v-btn>
+                            </v-flex>
+                        </v-card-text>
+                    </v-card>
+               </v-flex>     
+            </v-card> 
         </v-row>
     </v-content>
 </template>
 <script>
 export default {
-    data: () => ({
-        countries: ['Afghanistan', 'Albania', 'Algeria', 'Andorra', 'Angola', 'Anguilla', 'Antigua &amp; Barbuda', 'Argentina', 'Armenia', 'Aruba', 'Australia', 'Austria', 'Azerbaijan', 'Bahamas', 'Bahrain', 'Bangladesh', 'Barbados', 'Belarus', 'Belgium', 'Belize', 'Benin', 'Bermuda', 'Bhutan', 'Bolivia', 'Bosnia &amp; Herzegovina', 'Botswana', 'Brazil', 'British Virgin Islands', 'Brunei', 'Bulgaria', 'Burkina Faso', 'Burundi', 'Cambodia', 'Cameroon', 'Cape Verde', 'Cayman Islands', 'Chad', 'Chile', 'China', 'Colombia', 'Congo', 'Cook Islands', 'Costa Rica', 'Cote D Ivoire', 'Croatia', 'Cruise Ship', 'Cuba', 'Cyprus', 'Czech Republic', 'Denmark', 'Djibouti', 'Dominica', 'Dominican Republic', 'Ecuador', 'Egypt', 'El Salvador', 'Equatorial Guinea', 'Estonia', 'Ethiopia', 'Falkland Islands', 'Faroe Islands', 'Fiji', 'Finland', 'France', 'French Polynesia', 'French West Indies', 'Gabon', 'Gambia', 'Georgia', 'Germany', 'Ghana', 'Gibraltar', 'Greece', 'Greenland', 'Grenada', 'Guam', 'Guatemala', 'Guernsey', 'Guinea', 'Guinea Bissau', 'Guyana', 'Haiti', 'Honduras', 'Hong Kong', 'Hungary', 'Iceland', 'India', 'Indonesia', 'Iran', 'Iraq', 'Ireland', 'Isle of Man', 'Israel', 'Italy', 'Jamaica', 'Japan', 'Jersey', 'Jordan', 'Kazakhstan', 'Kenya', 'Kuwait', 'Kyrgyz Republic', 'Laos', 'Latvia', 'Lebanon', 'Lesotho', 'Liberia', 'Libya', 'Liechtenstein', 'Lithuania', 'Luxembourg', 'Macau', 'Macedonia', 'Madagascar', 'Malawi', 'Malaysia', 'Maldives', 'Mali', 'Malta', 'Mauritania', 'Mauritius', 'Mexico', 'Moldova', 'Monaco', 'Mongolia', 'Montenegro', 'Montserrat', 'Morocco', 'Mozambique', 'Namibia', 'Nepal', 'Netherlands', 'Netherlands Antilles', 'New Caledonia', 'New Zealand', 'Nicaragua', 'Niger', 'Nigeria', 'Norway', 'Oman', 'Pakistan', 'Palestine', 'Panama', 'Papua New Guinea', 'Paraguay', 'Peru', 'Philippines', 'Poland', 'Portugal', 'Puerto Rico', 'Qatar', 'Reunion', 'Romania', 'Russia', 'Rwanda', 'Saint Pierre &amp; Miquelon', 'Samoa', 'San Marino', 'Satellite', 'Saudi Arabia', 'Senegal', 'Serbia', 'Seychelles', 'Sierra Leone', 'Singapore', 'Slovakia', 'Slovenia', 'South Africa', 'South Korea', 'Spain', 'Sri Lanka', 'St Kitts &amp; Nevis', 'St Lucia', 'St Vincent', 'St. Lucia', 'Sudan', 'Suriname', 'Swaziland', 'Sweden', 'Switzerland', 'Syria', 'Taiwan', 'Tajikistan', 'Tanzania', 'Thailand', "Timor L'Este", 'Togo', 'Tonga', 'Trinidad &amp; Tobago', 'Tunisia', 'Turkey', 'Turkmenistan', 'Turks &amp; Caicos', 'Uganda', 'Ukraine', 'United Arab Emirates', 'United Kingdom', 'United States', 'Uruguay', 'Uzbekistan', 'Venezuela', 'Vietnam', 'Virgin Islands (US)', 'Yemen', 'Zambia', 'Zimbabwe'],
-        errorMessages: '',
-        name: null,
-        address: null,
-        city: null,
-        state: null,
-        zip: null,
-        country: null,
-        formHasErrors: false,
-    }),
-        computed: {
-            form () {
-            return {
-                name: this.name,
-                address: this.address,
-                city: this.city,
-                state: this.state,
-                zip: this.zip,
-                country: this.country,
-            }
-        },
-
-        watch: {
-                name () {
-                this.errorMessages = ''
-            },
-        methods: {
-    addressCheck () {
-      this.errorMessages = this.address && !this.name
-        ? 'Hey! I\'m required'
-        : ''
-
-      return true
-    },
-    resetForm () {
-      this.errorMessages = []
-      this.formHasErrors = false
-
-      Object.keys(this.form).forEach(f => {
-        this.$refs[f].reset()
-      })
-    },
-    submit () {
-      this.formHasErrors = false
-
-      Object.keys(this.form).forEach(f => {
-        if (!this.form[f]) this.formHasErrors = true
-
-        this.$refs[f].validate(true)
-        })
-    },
-    },
-  },
-  },
-
+    
 }
 </script>
