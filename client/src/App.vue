9e221e997f0332ef3f5df0b49ab4ba1a68e9e83c
Lucas Fernandes da Silva <47428909+Planctor@users.noreply.github.com>
2020-03-15 16:06:16 -0300

    Add files via upload
    ADICIONADO OS PRODUTOS, FICARAM ENTRE 8 E 16 ... DEPOIS VOTAR

--- client/src/App.vue
+++ client/src/App.vue
@@ -105,7 +105,7 @@
         <v-card-text class="white--text pt-0">
           Phasellus feugiat arcu sapien, et iaculis ipsum elementum sit amet. Mauris cursus commodo interdum. Praesent ut risus eget metus luctus accumsan id ultrices nunc. Sed at orci sed massa consectetur dignissim a sit amet dui. Duis commodo vitae velit et faucibus. Morbi vehicula lacinia malesuada. Nulla placerat augue vel ipsum ultrices, cursus iaculis dui sollicitudin. Vestibulum eu ipsum vel diam elementum tempor vel ut orci. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
         </v-card-text>
-          <a href="#" style="text-decoration:none;"><v-icon size="40px">mdi-facebook-box</v-icon></a>
+          <a href="#" style="text-decoration:none;"><v-icon size="40px">mdi-facebook</v-icon></a>
           <a href="#" style="text-decoration:none;"><v-icon size="40px">mdi-instagram</v-icon></a>
           <a href="#" style="text-decoration:none;"><v-icon size="40px">mdi-linkedin</v-icon></a>
           <a href="#" style="text-decoration:none;"><v-icon size="40px">mdi-twitter</v-icon></a>
