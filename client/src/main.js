2ca6df141c2c866c98a846e9f234eabcd97a28bd
Coutinho <@>
2020-04-20 18:31:26 -0300

    Criando Página para Rotas

--- client/src/main.js
+++ client/src/main.js
@@ -1,49 +1,16 @@
 import Vue from "vue";
 import App from "./App.vue";
 import VueRouter from "vue-router";
+//Dependencia das Rotas
+import routes from "./routes/routes";
 import vuetify from './plugins/vuetify';
 //Dependencia do Vuex
 import store from './store';
 //Import de páginas
-import adicionarItem from "./pages/adicionarItem";
-import Home from "./pages/home";
-import Leilao from "./pages/leilao";
-import Login from "./pages/login";
-import Criar from "./pages/criar";
-import Produtos from "./pages/Produtos";
+
 Vue.use(VueRouter);
 
-const router = new VueRouter({
-// rotas
-// precisa importar a página que vai ser referida a rota
-
-	routes:[
-		{
-			path:"/adicionarItem",
-			component:adicionarItem
-		},
-		{
-			path:"/criar",
-			component:Criar
-		},
-		{
-			path:"/login",
-			component:Login
-		},
-		{
-			path:"/",
-			component:Home
-		},
-		{
-			path:"/Produtos",
-			component:Produtos
-		},
-		{
-			path:"/leilao",
-			component:Leilao
-		}
-	]
-})
+const router = routes;
 
 Vue.config.productionTip = false;
 
