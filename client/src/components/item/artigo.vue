e0254df190032d907a86bea18fff22d203121525
jcode3001 <jc0d3001@outlook.com>
2020-05-11 10:52:00 -0300

    Ajuste dos componentes
    Artigo.vue -> pega de dados do banco.
    lance.vue -> lance sobre o artigo

--- client/src/components/item/artigo.vue
+++ client/src/components/item/artigo.vue
@@ -1,78 +1,93 @@
 <template>	
-	<v-container class="container">			
-		<v-card
-		class="mx-auto my-1"
-        max-width="350"
-		justify="center"
-        align="center"
-		>
+	<div>
+        <div class="col-md-6" style="float: left;">	
+			<v-card
+			max-width="100%"
+			height="420"
+			justify="center"
+			align="center"> 
+				<v-img
+				height="300"
+				width="100%"
+				v-bind:src="artigo.img"
+				/>
+                <br>
+                            
+				<v-row
+				align="center"
+				class="mx-0">
+					<v-rating
+					style="margin-left:2%"
+					v-bind:value="vendedor.value"
+					color="amber"
+					dense
+					half-increments
+					readonly
+					size="14"
+					/>
+					<div class="black--text ml-1">{{vendedor.value}} (Nota do vendedor)</div>
+				</v-row>
+
+				<v-row>
+					<a v-bind:href="artigo.link" target="_blank" style="margin-left:5%">link Youtube</a>
+				</v-row>
+
+			</v-card>
+		</div>
 		
-			<div v-if="aberto == false" class="my-1 subtitle-2 white--text" style="background:#3f51b5">
-                          Aberto
-                            </div>
+		<br>
+		
 
-                            <v-img
-                            height="200"
-                            src="https://i.imgur.com/EaowRPb.jpg"
-                            >
-                            <br>
-                            <br>
-                            <br>
-                            <br>
-                            <br>
-                            <br>
-                            <br>
-							<v-row
-                                align="center"
-                                class="mx-0"
-                                
-                            >
-                                <v-rating
-                                :value="4.5"
-                                color="amber"
-                                dense
-                                half-increments
-                                readonly
-                                size="14"
-                                float="bottom"
-                                ></v-rating>
+		<a href="#" style="text-decoration:none;float:right;margin-left:1%">  Próximo </a>  
+		<a href="#" style="text-decoration:none;float:right;margin-left:1%">  Anterior </a>
+		<a href="#"  style="text-decoration:none;float:right"> Voltar | </a>   
+		<br>     
+		<hr>
 
-                                <div class="white--text ml-4">4.5 (Nota do vendedor)</div>
-                            </v-row>
-							</v-img>
-							<br>
-							<div>
-                               <v-text> Carro novo e com pouco tempo de uso</v-text>
-                            </div>
-			<v-card-title class="subtitle-2 " >
-                            Lance Inicial:  <data style="margin:0px 1% 0px 1%"> R$ 30,00</data> 
-                            </v-card-title>
+		<p style="font-size:12px">
+			<b>visitas:</b>   
+			25
+		</p>
 
-			<v-card-title class="subtitle-2 " >
-                            Lance Atual:  <data style="margin:0px 1% 0px 1%"> </data> 
-                            </v-card-title>	
+		<div align="justify">
+			<v-text >{{ artigo.description }}</v-text>
+		</div>
+		<hr>
+		<p style="font-size:12px">
+		<b>Local:</b>   
+			Rio de Janeiro
+		</p>
+		<p class="subtitle-2">
+			<b> Dia dos leilão:</b>
+			<data style="margin:0px 1% 0px 1%"> {{ artigo.date }}</data>
+		</p>       
+		<hr>
+		<p class="subtitle-2 " >
+			Lance Inicial: 
+			<data style="margin:0px 1% 0px 1%">
+				{{ "R$ " + artigo.initialbid + ",00"}}
+			</data>
+		</p> 
 
+		<div class="col-md-6" style="float: right;">
+			
 
+				
 
-
-            <v-card v-if="aberto">
+			<v-card v-if="artigo.status">
 				<v-card-text class="red text-center">
 					<span class="white--text">Leilao Fechado</span>
 				</v-card-text>
 			</v-card>
-			<v-card id="card" v-else>
-				
+			<div id="card" v-else>
 				<lance/>
+			</div>
+		</div>
+		
 				
-			</v-card>
-
-			
-					
-	
-		</v-card>
-		<!-- abertura e fachamento de leilao beta -->
-		<v-btn v-on:click="closer(aberto)">leilao status</v-btn>
-	</v-container>
+	<!-- abertura e fachamento de leilao beta -->
+		<v-btn v-on:click="closer(artigo.status)">leilao status</v-btn>
+	</div>	
 </template>
 
 <script>
@@ -85,7 +100,19 @@ export default {
 	data(){
 		return{		
 			// teste botao de abrir leilao
-			aberto:false,			
+			
+			artigo: {
+				name:"Carro",
+				img:"https://firebasestorage.googleapis.com/v0/b/portalleilao-26290.appspot.com/o/Files%2FArticles%2Farticles%20Portal%20Leiloes%20from%20Sun%20May%2003%202020%2011%3A15%3A23%20GMT-0300%20(Brasilia%20Standard%20Time)?alt=media&token=74d2e610-c010-42c5-93a5-9e0734a19117",
+				description:"Carro novo e com pouco tempo de uso",
+				link: "https://www.youtube.com/watch?v=kc0Q9sfbhwg",
+				date:"30/06/2020",
+				initialbid:55.00,
+				status:false,				
+			},
+			vendedor: {
+				value: 5
+			}			
 		};
 	},
 	
@@ -93,9 +120,9 @@ export default {
 		// Teste
 		closer(modal){
 			if (modal) {
-				this.aberto = false;
+				this.artigo.status = false;
 			}else {
-				this.aberto = true;
+				this.artigo.status = true;
 			}			
 		}
 		
@@ -108,6 +135,3 @@ export default {
 		padding: 30px;
 	}
 </style>
-
-
-
