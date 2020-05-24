8b6fb4028440662540bb4c0f745f83a7a450193f
jcode3001 <jc0d3001@outlook.com>
2020-05-24 11:22:04 -0300

    ajustado a page

---
+++ client/src/pages/criarconta.vue
@@ -0,0 +1,35 @@
+<template>
+    <v-content>
+        <v-row justify="center">
+            <v-card color="grey" max-width="1000" class="mb-12"  :elevation="10" width="800px">
+                <v-flex class="d-flex ">
+                <!--CADASTRO POR API-->   
+                    <v-card
+                    width="50%"
+                    height="530px"
+                    color="blue"  
+                    >               
+                        <p class="white--text headline text-center">
+                            CRIE SUA CONTA, FÁCIL...
+                        </p>
+                        <v-row justify="center fill-height" >
+                            <v-icon size="50px" color="white" class="mr-5">mdi-facebook</v-icon>
+                            <v-icon size="50px" color="white" class="mr-5">mdi-google</v-icon>
+                            <v-icon size="50px" color="white">mdi-linkedin</v-icon>
+                        </v-row>                    
+                    </v-card >
+                    <formulario/>
+                </v-flex>     
+            </v-card> 
+        </v-row>
+    </v-content>
+</template>
+
+<script>
+    import formulario from "../components/item/cadastroCliente"
+    export default {
+        components:{
+            formulario
+        }
+    }
+</script>
