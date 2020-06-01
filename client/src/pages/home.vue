a27da0795026a138063a8977b99ea0b220265869
Lucas Fernandes da Silva <Lucas_201130@hotmail.com>
2020-06-01 15:59:07 -0300

    removendo botao de scroll

--- client/src/pages/home.vue
+++ client/src/pages/home.vue
@@ -149,7 +149,7 @@
                                <p class="body-1">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec sagittis elementum tellus sit amet mattis. Vestibulum vel mi sit amet arcu bibendum gravida. Ut nec tortor et nulla tristique scelerisque. Cras quis ornare libero. Donec efficitur id odio quis efficitur. Suspendisse commodo diam sit amet pulvinar pretium. Donec consectetur eros sed quam tristique, non scelerisque ante posuere. Integer sed semper risus. Nullam euismod aliquet odio, ac dignissim nunc maximus nec. Proin a ex nec ex sodales bibendum sit amet eget ex. Curabitur ultrices urna sed massa tempus blandit. Proin dictum porta diam in convallis. Nulla ultrices felis ante, in dapibus leo convallis in.Vestibulum quis sem eu augue feugiat rhoncus. Curabitur ornare nisi elit, sed sagittis ante aliquet a. Nulla cursus eget tortor quis congue. Cras lorem enim, malesuada sed venenatis non, condimentum quis massa. In ultrices scelerisque feugiat. Aenean volutpat porttitor nibh, quis varius augue pretium id. Nunc malesuada arcu vitae velit semper dignissim. Sed placerat erat sed cursus feugiat. Mauris a pellentesque elit, in vestibulum urna. Praesent non aliquet nibh, et lacinia leo. Duis at lectus vel quam sagittis egestas. Pellentesque quis placerat ex. Cras convallis mollis pulvinar. Sed sit amet ligula turpis. Morbi ullamcorper odio vitae velit.</p>
                                 <v-btn
                                     rounded
-                                    color="red"
+                                    color="#a64e4b"
                                     x-large
                                     class="white--text">
                                     Ler Mais
@@ -235,13 +235,14 @@
                                     </v-text-field>
                                 </v-row>
                                 <v-btn
-                                class="mt-1 ml-6 white--text"
-                                color="#422321"
+                                class="ml-6 white--text"
+                                color="#a64e4b"
                                 large
                                 rounded
                                 >
                                     Enviar    
                                 </v-btn>
+                                
                             </v-col>    
                            
                         </v-col>
@@ -299,25 +300,7 @@
                             </v-row>     
                         </v-col>
                     </v-card>
-                    <!--botao de subida-->
-                    <v-row
-                    align="end"
-                    justify="end"
-                    class=" mr-3"
-                    >
-                        <v-btn
-                        absolute
-                        fab
-                        outlined
-                        x-large
-                        align="end"
-                        color="white"
-                        class="hidden-sm-only hidden-xs-only"
-                        @click="$vuetify.goTo(-5)"
-                        >
-                            <v-icon>fas fa-chevron-up</v-icon>
-                        </v-btn>
-                    </v-row>      
+                          
     </v-app> 
 </template>
 
