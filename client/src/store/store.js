4bebaa644274b0c5c9c752186803db72b10da55e
Pedro Coutinho <coutinhopedro111@gmail.com>
2020-06-13 03:10:33 -0300

    limpando store

--- client/src/store/store.js
+++ client/src/store/store.js
@@ -1,23 +1,67 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import * as firebase from 'firebase'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-
-    account: undefined,
-    logged: false,
+    user:null,
+    loading:false,
+    error:null
   },
-
+  user:{
+id:'sdadasdasd',
+registered:['dssadadsa']
+  },
   mutations: {
-    logged(account){
-      this.state.account = account;
-      this.state.logged = true
+    setUser(state,payload){
+      state.user = payload
     }
   },
   actions: {
+    signUserUp({commit},payload){
+      firebase.auth().createUserWithEmailAndPassword(payload.email, payload.senha)
+      .then(
+         user => {
+           const newUser = {
+             id:user.uid,
+             registered:[],
+             
+           }
+           commit('setUser',newUser)
+        }
+     )
+     .catch(
+       error => {
+         console.log(error)
+       }
+     )
+    },
+    signUserIn({commit},payload){
+      firebase.auth().signInWithEmailAndPassword(payload.email, payload.senha)
+      .then(
+        user => {
+          const newUser = {
+            id:user.uid,
+            registered:[],
+          }
+          commit('setUser',newUser)
+        }
+      )
+      .catch(
+        error => {
+          console.log(error)
+        }      
+      )
+    }
+
   },
   modules: {
+  },
+  getters:{
+    user(state){
+      return state.user
+    }
   }
 })
