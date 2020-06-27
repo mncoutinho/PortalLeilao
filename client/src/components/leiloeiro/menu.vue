8c68d1a8db328c6886b71bde7b311b779508e343
z <jc0d3001@outlook.com>
2020-06-27 18:57:54 -0300

    fixado da branch do Lucas 27-06

---
+++ client/src/components/leiloeiro/menu.vue
@@ -0,0 +1,81 @@
+<template>
+  <v-app id="inspire">
+        <v-navigation-drawer permanent expand-on-hover>
+          <!--INFOMAÇOES DO USUARIO-->
+          <v-list :key="usuario.length" v-for="usuario in usuario">
+            <v-list-item class="px-2">
+              <v-list-item-avatar>
+                <v-img src="https://cdn.shopify.com/s/files/1/0946/5368/products/alho_grande.jpg?v=1440442906"/>
+              </v-list-item-avatar>
+              <v-list>
+                <v-list-item-title class="title">{{usuario.name}}</v-list-item-title>
+                <v-list-item-subtitle class="title">{{usuario.email}}</v-list-item-subtitle>
+              </v-list>
+            </v-list-item>
+          </v-list>
+          <v-divider />
+          <!--CATEGORIAS-->
+          <v-list 
+            nav 
+            dense 
+            :key="pai.title" 
+            v-for="pai in conteudo">
+            <!--titulo-->
+              <v-list-group>
+                <template v-slot:activator>
+                  <v-list-item-icon>
+                    <v-icon>{{pai.icon}}</v-icon>
+                  </v-list-item-icon>
+                  <v-list-item-title v-text="pai.title"></v-list-item-title>
+                </template>
+                <!--conteudo-->
+                  <v-list-item-content 
+                    dense 
+                    :key="filho.conteudo" 
+                    v-for="filho in pai.conteudo">
+                        <v-btn text v-text="filho.title" :to="filho.routerLinks"></v-btn>
+                  </v-list-item-content>
+              </v-list-group>
+              <v-divider class="mt-2"/>
+          </v-list>
+        </v-navigation-drawer>
+  </v-app>
+</template>
+
+<script>
+export default { 
+  data:() =>({
+    usuario: [{ name: "Lucas", email: "Lili@empresa.com" }],
+    conteudo:[
+      //numero 1
+      {icon:"mdi-home",
+        title:"NOME",
+        conteudo:[
+          {title:"nome", routerLinks:'/adicionarItem'},
+          {title:"nome", routerLinks:'/adicionarItem'},
+          {title:"nome", routerLinks:'/adicionarItem'},
+        ]
+      },
+      //numero 2
+      {icon:"mdi-home",
+        title:"OUTRO-NOME",
+        conteudo:[
+          {title:"nomeS"},
+          {title:"nomeS"},
+          {title:"nomeS"},
+        ]
+      },
+      //numero 3
+      {icon:"mdi-home",
+        title:"FUNCINO POHA",
+        conteudo:[
+          {title:"AEW CARALHO"},
+          {title:"PEGOU"},
+          {title:"kek"},
+        ]
+      },
+
+    ] 
+  })
+};
+</script>
--- client/src/pages/leiloeiroPage.vue
+++ client/src/pages/leiloeiroPage.vue
@@ -1,53 +1,20 @@
 <template>
-  <v-navigation-drawer permanent expand-on-hover>
-    <!--INFOMAÇOES DO USUARIO-->
-    <v-list :key="usuario.length" v-for="usuario in usuario">
-      <v-list-item class="px-2">
-        <v-list-item-avatar>
-          <v-img
-            src="https://cdn.shopify.com/s/files/1/0946/5368/products/alho_grande.jpg?v=1440442906"
-          />
-        </v-list-item-avatar>
-        <v-list>
-          <v-list-item-title class="title">{{usuario.name}}</v-list-item-title>
-          <v-list-item-subtitle class="title">{{usuario.email}}</v-list-item-subtitle>
-        </v-list>
-      </v-list-item>
-    </v-list>
-    <v-divider />
-    <!--CATEGORIAS-->
-    <v-list nav dense :key="conteudo.length" v-for="conteudo in conteudo">
-      <!--titulo-->
-      <v-list-group>
-        <template v-slot:activator>
-          <v-list-item-icon>
-            <v-icon>{{conteudo.icon}}</v-icon>
-          </v-list-item-icon>
-          <v-list-item-title>{{conteudo.title}}</v-list-item-title>
-        </template>
-        <!--conteudo-->
-        <v-list-item-action dense :key="content" v-for="content in conteudo.content">
-          <v-list-item-title v-text="content[0]"></v-list-item-title>
-          <v-list-item-title v-text="content[1]"></v-list-item-title>
-          <v-list-item-title v-text="content[2]"></v-list-item-title>
-        </v-list-item-action>
-      </v-list-group>
-    </v-list>
-  </v-navigation-drawer>
+  <v-app id="inspire">
+    <v-row no-gutters>
+      <navegacao/>
+      <v-col>
+        <h1>TEXTO</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe nulla vitae obcaecati laudantium! Aperiam sit et consectetur minima animi accusamus sapiente quas asperiores. Blanditiis, cumque.</p>
+      </v-col>
+    </v-row>
+  </v-app>  
 </template>
 
 <script>
+import navegacao from "../components/leiloeiro/menu"
 export default {
-  data() {
-    return {
-      usuario: [{ name: "Lucas", email: "Lili@empresa.com" }],
-      conteudo: [
-        { icon: "fas fa-gavel", title: "Leilao", content:["1","2","3"]},
-        { icon: "fas fa-file-alt", title: "Artigos" , content:["test","2","3"]},
-        { icon: "mdi-card-account-details", title: "Usuarios", content:["criar","procurar","lista Negra"] },
-        { icon: "fas fa-ad", title: "Anuncios" }
-      ]
-    };
+  components:{
+    navegacao,
   }
-};
+}
 </script>
