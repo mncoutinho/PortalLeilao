3ea7c1a02d615972d1b44fd48975e20b3a37e86e
Lucas Fernandes da Silva <Lucas_201130@hotmail.com>
2020-09-02 14:35:35 -0300

    adicionando rota ao item com link

--- client/src/components/home/getLivesActives.vue
+++ client/src/components/home/getLivesActives.vue
@@ -1,7 +1,6 @@
 <template>
-        <v-flex 
-            class="mt-auto"
-        >
+      <!-- BOX-->
+        <v-flex class="mt-auto">
             <v-card 
             max-width="1550"
             class="mx-auto"
@@ -21,8 +20,9 @@
                         </p>
                     </v-col>
                 </v-row >
+                <!--TRANSMISSOES-->
                     <v-hover 
-                    v-slot:default="{hover}"
+                    v-slot:default="{ hover }"
                     open-delay="100">  
                         <v-row 
                         align="center" 
@@ -30,66 +30,30 @@
                         class="mt-12"
                         >
                             <v-card
-                                class="mt-6 mb-6"
-                                style="cursor:pointer"
-                                width="330"
-                                :elevation="hover ? 16 : 2"
-                                v-for="stream in limitador"
-                                :key="stream.nome"
+                            class="mt-6 mb-6"
+                            style="cursor:pointer"
+                            width="330"
+                            :elevation="hover ? 16 : 2"
+                            v-for="stream in limitador"
+                            :key="stream.nome"
                             >
-                                <v-img 
-                                    width="100%" 
-                                    height="300" 
-                                    :src="stream.imgUrl[0]" 
+                                <v-img width="100%" height="300" :src="stream.imgUrl[0]" 
                                 /> 
-                                <v-list-item-content 
-                                    class="ml-5"
-                                >
+                                <v-list-item-content class="ml-5">
                                     <div>     
-                                        <span 
-                                            style="color:green"
-                                        >
-                                            {{status(stream.active)}}
-                                        </span>
-
-                                        <v-list-item-title 
-                                            style="color:#63432D" 
-                                            class="bold headline mb-1"
-                                        >
-                                            {{stream.name}}
-                                        </v-list-item-title>
-
-                                        <v-list-item-subtitle 
-                                            style="color:#1B120C" 
-                                            class="mb-2"
-                                        >
-                                            {{stream.description}}
-                                        </v-list-item-subtitle>
+                                        <span :style="color(stream.active)" >{{status(stream.active)}}</span>
+                                        <v-list-item-title style="color:#63432D" class="bold headline mb-1">{{stream.name}}</v-list-item-title>
+                                        <v-list-item-subtitle style="color:#1B120C" class="mb-2">{{stream.description}}</v-list-item-subtitle>
                                             <v-divider class="mx-5" color="#EDE7E2"/>
                                 </div>            
-                                <v-row 
-                                    class="mr-5" 
-                                    justify="center"
-                                >
-                                    <v-btn 
-                                        outlined 
-                                        rounded 
-                                        class="pr-12 pl-12 mt-4 mb-2" 
-                                        color="green" 
-                                        @click="logado"
-                                    >
-                                        PARTICIPAR
-                                    </v-btn>
+                                <v-row class="mr-5" justify="center">
+                                    <v-btn outlined rounded class="pr-12 pl-12 mt-4 mb-2" :style="color(stream.active)" @click="logado(stream.id)">{{status(stream.active)}}</v-btn>
                                 </v-row>
                                 </v-list-item-content>   
                             </v-card>
                         </v-row>
                     </v-hover>
-                <v-row 
-                    align="center" 
-                    justify="space-around" 
-                    class="mt-12"
-                >
+                <v-row align="center" justify="space-around" class="mt-12">
                 </v-row>
             </v-card>
         </v-flex>
@@ -110,16 +74,24 @@ export default {
     methods:{
         status(status){
             if(status){
-              return "Ao vivo"
+              return "Participar"
             }else{
                return "Finalizado" 
             }
         },
-        logado(){
+        color(status){
+            if(status){
+              return "color:green"
+            }else{
+               return "color:red" 
+            }
+        },
+        logado(id){
             if(!this.user.email){
                 return this.$router.push('/login')
             }else{
-                return this.$router.push('/produtos')
+                this.target = id 
+                return this.$router.push({path:'/leilao', query:{id:this.target}})
             }
         }    
     },
