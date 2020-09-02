c9fab2b694d746f80341e061dac03b6d65ed5a2f
Lucas Fernandes da Silva <Lucas_201130@hotmail.com>
2020-09-02 14:36:23 -0300

    adicionando componentes

--- client/src/App.vue
+++ client/src/App.vue
@@ -4,6 +4,17 @@
     <v-main>
         <router-view/>
     </v-main>
+    <v-snackbar
+          v-model="msg.mostra"
+          :color="msg.cor"
+          top
+          :timeout="msg.tempo"
+          class="pa-2"
+        >
+          <v-row justify="center">
+            <h3 style="text-weight:0">{{msg.text}}</h3>
+          </v-row>
+        </v-snackbar>
     <Footer/>
   </v-app>
 </template>
@@ -11,12 +22,19 @@
 import NavBar from './components/layout/navbar';
 import Footer from './components/layout/footer';
 export default {
-
   name: 'App',
   components:{
     NavBar,
     Footer
-  }
+  },
+    data:()=>({
+    mensagem: true,
+  }),
+  computed:{
+    msg(){
+      return this.$store.state.alerts
+    }
+  },
 }
 </script>
 
