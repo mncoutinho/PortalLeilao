a601a61bdb00ece6385b48218f0c7492951cdb4a
Lucas Fernandes da Silva <Lucas_201130@hotmail.com>
2020-09-02 14:36:49 -0300

    botando pra funcionar

--- client/src/store/modules/alerts.js
+++ client/src/store/modules/alerts.js
@@ -1,76 +1,76 @@
 export default {
-        state: {
-            text: 'texto',
-            cor: '',
-            tempo: '',
-            mostra: false,
+    state: {
+        text: 'texto',
+        cor: '',
+        tempo: '',
+        mostra: false,
+    },
+    mutations: {
+        ERRO(state) {
+            state.text = 'erro',
+            state.cor = 'red',
+            state.tempo = 4000,
+            state.mostra = true
         },
-        mutations: {
-            erro(state) {
-                state.text = 'erro',
-                    state.cor = 'red',
-                    state.tempo = 4000,
-                    state.mostra = true
-            },
-            weelcome(state) {
-                state.text = 'Seja Bem-Vindo',
-                    state.cor = 'blue',
-                    state.tempo = 4000,
-                    state.mostra = true
-            },
-            verificarEmail(state) {
-                state.text = 'Verifique o seu e-mail',
-                    state.cor = 'red',
-                    state.tempo = 4000,
-                    state.mostra = true
-            },
-            usuarioDeslogar(state) {
-                state.text = 'Usúario deslogou',
-                    state.cor = 'red',
-                    state.tempo = 5000,
-                    state.mostra = true
-            },
-            VERIFICAR_CAIXA(state) {
-                state.text = 'Verifique sua caixa de email para altera sua senha',
-                    state.cor = 'red',
-                    state.tempo = 5000,
-                    state.mostra = true
-            },
-            CADASTRADO_SUCESSO(state) {
-                state.text = 'Cadastrado com Sucesso',
-                    state.cor = 'red',
-                    state.tempo = 5000,
-                    state.mostra = true
-            },
-            ALTERADO_SUCESSO(state) {
-                state.text = 'alterado com sucesso',
-                    state.cor = 'green',
-                    state.tempo = 5000,
-                    state.mostra = true
-            },
-            ALGO_INESPERADO(state, payload) {
-                state.text = 'Algo inesperado aconteceu:' + payload,
-                    state.cor = 'red',
-                    state.tempo = 5000,
-                    state.mostra = true
-            },
-            DELETADO(state) {
-                state.text = 'Deletado com Sucesso',
-                    state.cor = 'green',
-                    state.tempo = 5000,
-                    state.mostra = true
-            },
-            MOSTRAR_CONTEUDO(state, payload) {
-                state.text = payload,
-                    state.cor = 'green',
-                    state.tempo = 5000,
-                    state.mostra = true
-            },
-            ATUALIZADO_SUCESSO(state, payload) {
-                state.text = payload,
-                    state.cor = 'green',
-                    state.tempo = 5000,
-                    state.mostra = true
-            },
+        VERIFICAR_EMAIL(state) {
+            state.text = 'Verifique o seu e-mail',
+            state.cor = 'red',
+            state.tempo = 4000,
+            state.mostra = true
         },
+        USUARIO_DESLOGAR(state) {
+            state.text = 'Usúario deslogou',
+            state.cor = 'red',
+            state.tempo = 5000,
+            state.mostra = true
+        },
+        VERIFICAR_CAIXA(state) {
+            state.text = 'Verifique sua caixa de email para altera sua senha',
+            state.cor = 'red',
+            state.tempo = 5000,
+            state.mostra = true
+        },
+        CADASTRADO_SUCESSO(state) {
+            state.text = 'Cadastrado com Sucesso',
+            state.cor = 'red',
+            state.tempo = 5000,
+            state.mostra = true
+        },
+        ALTERADO_SUCESSO(state) {
+            state.text = 'alterado com sucesso',
+            state.cor = 'green',
+            state.tempo = 5000,
+            state.mostra = true
+        },
+        ALGO_INESPERADO(state, payload) {
+            state.text = 'Algo inesperado aconteceu:' + payload,
+            state.cor = 'red',
+            state.tempo = 5000,
+            state.mostra = true
+        },
+        DELETADO(state) {
+            state.text = 'Deletado com Sucesso',
+            state.cor = 'green',
+            state.tempo = 5000,
+            state.mostra = true
+        },
+        MOSTRAR_CONTEUDO(state, payload) {
+            state.text = payload,
+            state.cor = 'green',
+            state.tempo = 5000,
+            state.mostra = true
+        },
+        ATUALIZADO_SUCESSO(state, payload) {
+            state.text = payload,
+            state.cor = 'green',
+            state.tempo = 5000,
+            state.mostra = true
+        },
+        POSSUI_MENSAGEM(state){
+            state.text = 'tem uma mensagem',
+            state.cor = 'green',
+            state.tempo = 5000,
+            state.mostra = true
+        }
+    },
 }
