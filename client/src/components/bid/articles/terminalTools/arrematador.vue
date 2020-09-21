f79092decfbb474872872a866a527f2069f3268e
Jose Paiva <jc0d3001@outlook.com>
2020-09-21 10:47:03 -0300

    novos botoes adicionado lucas colori pra mim

--- client/src/components/bid/articles/terminalTools/arrematador.vue
+++ client/src/components/bid/articles/terminalTools/arrematador.vue
@@ -5,15 +5,55 @@
 				Lance Atual: {{"R$"+ lanceNow+",00" }}
 			</strong>
 		</v-card-title>
-            <v-btn
-				width="100%"
-				color="red"
-				class="white--text"
-				large
-                v-on:click="arremate()"
-            >
-                {{button(item.active)}}
-            </v-btn>
+			<div>
+				<h4>Painel de operacoes</h4>
+				<div>
+					<h3>operadores</h3>
+					<v-btn
+					width="50%"
+					color="blue"
+					class="white--text"
+					v-on:click="alert('fechamento')"
+					large
+					>
+						Aviso Fechamento
+					</v-btn>
+
+					<v-btn
+					width="50%"
+					color="red"
+					class="white--text"
+					v-on:click="alert('martelo')"
+					large
+					>
+						Aviso Martelo
+					</v-btn>
+				</div>
+
+				<div>
+					<h3>Controles sobre o Lote</h3>
+					<v-btn
+					width="50%"
+					color="yellow"
+					class="black--text"
+					large
+					>
+						Martelo Automatico
+					</v-btn>
+
+					<v-btn
+					width="50%"
+					:color="color"
+					class="white--text"
+					large
+					v-on:click="arremate()"
+				>
+					{{button(item.active)}}
+				</v-btn>
+				</div>
+				
+			</div>
+            
 	</v-card>		
 </template>
 
@@ -23,6 +63,7 @@ export default {
     data() {
         return {
 			status: "",
+			color:"",
         }
     },
     computed:{
@@ -59,11 +100,32 @@ export default {
 		},
 		button(item) {
 			if(item){
+				this.color = "red"
 				return this.status = "fechar lote" 
 			}else{
+				this.color = "green"
 				return this.status = "abrir lote" 
 			}
 		},
+		alert(type){
+			const time = new Date();
+			let msg = {
+                time: `${time.getDay()}/${time.getMonth()}/${time.getFullYear()}-${time.getHours()}:${time.getMinutes()}`
+            }
+			switch (type) {
+				case 'fechamento':
+					msg.text = "vc ira fechar o lote"
+					break;
+				case 'martelo':
+					msg.text = "vc avisara q estara batendo o martelo"
+					break;
+				default:
+					msg.text = "error"
+					break;
+			}
+			this.$store.dispatch('addInfo',{info: msg, id: this.$route.query.id})
+			
+		}
     }
 }
 </script>
