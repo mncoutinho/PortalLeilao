82ea14cfcce7340b09854e527f4b09f471d5d308
Jose Paiva <jc0d3001@outlook.com>
2020-09-22 14:19:06 -0300

    tabela feita

--- client/src/components/leiloeiro/malaDireta/getSearch.vue
+++ client/src/components/leiloeiro/malaDireta/getSearch.vue
@@ -1,6 +1,62 @@
 <template>
     <div>
-        {{result}}
+        <v-card-text>
+            <div v-if="result.length" >
+                <small 
+                    justify="center"
+                >
+                    <span 
+                        class="badge badge-info"
+                    >
+                        {{result.length}}
+                    </span> 
+                    resultados encontrados
+                </small>
+
+                <v-row>
+                    <v-col>
+                        Peca
+                    </v-col>
+                    <v-col>
+                        usuario
+                    </v-col>
+                    <v-col>
+                        data
+                    </v-col>
+                    <v-col>
+                        lance
+                    </v-col>
+                </v-row>
+
+                <v-row 
+                    v-for="result in result" 
+                    :key="result.length"
+                >
+                    <v-col>
+                        <small v-text="result.peca"/>
+                    </v-col>
+                    <v-col>
+                        <small v-text="result.user"/>
+                    </v-col>
+                    <v-col>
+                        <small v-text="result.time"/>
+                    </v-col>
+                    <v-col>
+                        <small v-text="result.lance"/>
+                    </v-col>
+                </v-row>
+            </div>
+                      
+            <div v-else>
+                <small >
+                    Sem resultado
+                </small>
+            </div>
+			
+        </v-card-text>
+
+        
+		
     </div>
 </template>
 
@@ -14,3 +70,4 @@ export default {
     },
 }
 </script>
+
