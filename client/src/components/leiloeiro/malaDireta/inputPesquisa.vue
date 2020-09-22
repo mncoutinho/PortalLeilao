439d83cbc9804878b18b420ad495dec9bfecf9b7
Jose Paiva <jc0d3001@outlook.com>
2020-09-22 10:30:17 -0300

    feito o input

---
+++ client/src/components/leiloeiro/malaDireta/inputPesquisa.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <v-text-field
+            v-model="text" 
+            label="digite o que procura"
+        />  
+        <v-btn
+            @click="search(text)"
+            color="brown"
+            class="white--text ml-12"            
+        >
+            procurar
+        </v-btn>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            text:""
+        }
+    },
+    methods: {
+        search(text){
+            alert("procurando por "+text)
+        }
+    },
+
+}
+</script>
--- client/src/pages/gestor.vue
+++ client/src/pages/gestor.vue
@@ -3,11 +3,15 @@
         <h1>
             teste
         </h1>
+        <pesquisa/>
     </div>
 </template>
 
 <script>
+import pesquisa from "../components/leiloeiro/malaDireta/inputPesquisa"
 export default {
-
+components:{
+        pesquisa
+    },
 }
 </script>
