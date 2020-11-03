56296c4abf6874aee769312b90aa31c6799600c6
Jose Paiva <jc0d3001@outlook.com>
2020-11-03 09:29:21 -0300

    organizado o router

--- client/src/routes/routes.js
+++ client/src/routes/routes.js
@@ -1,45 +1,24 @@
 import VueRouter from "vue-router";
 import leiloeiroPage from "../pages/leiloeiroPage";
 import logout from "../pages/logout";
-import terminal from "../pages/terminal"
-import gestor from "../pages/gestor"
 import Home from "../pages/home";
 import Leilao from "../pages/leilao";
 import Login from "../pages/login";
 import Criar from "../pages/criarconta";
 import userpage from "../pages/userpage.vue";
-import adicionarLeilao from "../pages/addLeilao";
 import Produtos from "../pages/Produtos";
-import adicionarItem from "../pages/adicionarItem";
-import updateItem from "../pages/updateItem";
-import meusProdutos from "../pages/meusProdutos"
-import updateLeilao from "../pages/updateLeilao"
+
 
 const router = new VueRouter({
     // rotas
     // precisa importar a página que vai ser referida a rota
     
-        routes:[
-            {
-                path:"/adicionarItem",
-                name:"adicionarItem",
-                component:adicionarItem
-            },
-            {
-                path:"/updateItem",
-                name:"updateItem",
-                component:updateItem
-            },
+        routes:[        
             {
                 path:"/Produtos",
                 name:"Produtos",
                 component:Produtos
-            },
-            {
-                path:"/meusProdutos",
-                name:"meusProdutos",
-                component:meusProdutos
-            },
+            },            
             {
                 path:"/criar",
                 name:"criar",
@@ -56,8 +35,7 @@ const router = new VueRouter({
                 name:"Home",
                 component:Home,
                 
-            },
-            
+            },           
             {
                 path:"/leilao",
                 name:"leilao",
@@ -69,16 +47,6 @@ const router = new VueRouter({
                 component:userpage
             },
             {
-                path:"/addLeilao",
-                name:"addLeilao",
-                component:adicionarLeilao
-            },
-            {
-                path:"/updateLeilao",
-                name:"updateLeilao",
-                component:updateLeilao
-            },
-            {
                 path:"/leiloeiro",
                 name:"leiloeiroPage",
                 component:leiloeiroPage
@@ -88,16 +56,6 @@ const router = new VueRouter({
                 name:"sair",
                 component:logout
             },
-        {
-            path: "/terminal",
-            name: "terminal",
-            component: terminal
-        },
-        {
-            path:"/gestor",
-            name:"gestor",
-            component:gestor
-        }
         ]
     });
 
