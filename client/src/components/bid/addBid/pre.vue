edbd8a51ae983c02d1661db587484326efedb2e7
Lucas Fernandes da Silva <Lucas_201130@hotmail.com>
2020-11-03 14:57:56 -0300

    aplicando formas de pagamento

--- client/src/components/bid/addBid/pre.vue
+++ client/src/components/bid/addBid/pre.vue
@@ -4,16 +4,39 @@
       flat>
         <v-row
         justify="center">
-          <v-card>
-            <v-btn
-              color=#562B28
-              center
-              class="white--text"
-              depressed
-              large
-              v-text="'Prosseguir'"
-              @click="next()"
-              ></v-btn>
+          <v-card
+          width="600px"
+          class="pa-10"
+          >
+              <v-col>
+                <!--formas de pagamentos-->
+                <v-container>
+                  <h3>Formas de pagamento:</h3>
+                  <v-row class="d-flex flex-wrap justify-space-between">
+                    <v-radio-group 
+                    v-model="radioGroup"
+                    v-for="radio in radios"
+                    :key="radio.length"
+                    >
+                        <v-icon size="50" color=#562B28>{{radio.icon}}</v-icon>
+                        <v-radio
+                          color=#562B28
+                          :label="radio.nome"
+                          :value="radio.valor"
+                        ></v-radio>
+                    </v-radio-group>
+                  </v-row>
+                </v-container>
+                <v-btn
+                  color=#562B28
+                  center
+                  class="white--text"
+                  depressed
+                  large
+                  v-text="'Prosseguir'"
+                  @click="next()"
+                  ></v-btn>
+                </v-col>
           </v-card>
         </v-row>
       </v-card>
@@ -22,6 +45,16 @@
 
 <script>
 export default {
+  data(){
+    return{
+      radioGroup: 'banco',
+      radios:[
+        {icon:'fas fa-barcode',nome:'Boleto Bancário', valor:'banco'},
+        {icon:'fas fa-credit-card',nome:'Cartão', valor:'cartão'},
+        {icon:'fab fa-cc-paypal',nome:'PayPal', valor:'pay'},
+      ]
+    }
+  },
   methods:{
     next(){
       this.$store.commit('ADD')
