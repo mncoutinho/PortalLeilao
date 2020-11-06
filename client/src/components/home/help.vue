47dd62c2060071ec864819baa16e7c892c9ce6e4
Lucas Fernandes da Silva <Lucas_201130@hotmail.com>
2020-11-05 22:00:05 -0300

    Correçoes visuais no layout

--- client/src/components/home/help.vue
+++ client/src/components/home/help.vue
@@ -4,10 +4,6 @@
         class="hidden-sm-only hidden-xs-only">
             <div
             style="width:100%">
-               <!-- <v-img
-                style="opacity: 0.8"
-                src="" 
-                > -->
                     <v-col 
                     cols="12"
                     md="6"
@@ -18,14 +14,13 @@
                         <p style="color:#422321" class="headline">Veja as dicas dos nossos especialistas</p>
                         <p class="body-1">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec sagittis elementum tellus sit amet mattis. Vestibulum vel mi sit amet arcu bibendum gravida. Ut nec tortor et nulla tristique scelerisque. Cras quis ornare libero. Donec efficitur id odio quis efficitur. Suspendisse commodo diam sit amet pulvinar pretium. Donec consectetur eros sed quam tristique, non scelerisque ante posuere. Integer sed semper risus. Nullam euismod aliquet odio, ac dignissim nunc maximus nec. Proin a ex nec ex sodales bibendum sit amet eget ex. Curabitur ultrices urna sed massa tempus blandit. Proin dictum porta diam in convallis. Nulla ultrices felis ante, in dapibus leo convallis in.Vestibulum quis sem eu augue feugiat rhoncus. Curabitur ornare nisi elit, sed sagittis ante aliquet a. Nulla cursus eget tortor quis congue. Cras lorem enim, malesuada sed venenatis non, condimentum quis massa. In ultrices scelerisque feugiat. Aenean volutpat porttitor nibh, quis varius augue pretium id. Nunc malesuada arcu vitae velit semper dignissim. Sed placerat erat sed cursus feugiat. Mauris a pellentesque elit, in vestibulum urna. Praesent non aliquet nibh, et lacinia leo. Duis at lectus vel quam sagittis egestas. Pellentesque quis placerat ex. Cras convallis mollis pulvinar. Sed sit amet ligula turpis. Morbi ullamcorper odio vitae velit.</p>
                         <v-btn
-                            rounded
-                            color="#a64e4b"
-                            x-large
-                            class="white--text">
-                            Ler Mais
+                        rounded
+                        x-large
+                        color="#a64e4b"
+                        class="white--text"
+                        v-text="'Leia Mais...'">
                         </v-btn>
                     </v-col>
-                <!-- </v-img>    -->
             </div>
         </v-row>
 </template>
--- client/src/components/home/registreEmail.vue
+++ client/src/components/home/registreEmail.vue
@@ -1,31 +1,31 @@
 <template>
-  <div
-        style="background-color:#EFEAEA"
-        class="mt-4">
-            <v-row >
-                <v-col align="center" class="mt-4">
+    <div>
+        <v-row>
+            <v-col align="center" class="mt-4">
+                <v-col cols="6">
                     <p class="display-1">Fique por dentro das ultimas novidades.</p>
-                    <v-col cols="10" sm="5" class="mt-6">
-                        <v-row>   
-                            <v-text-field
-                            solo
-                            label="Email">
-                            </v-text-field>
-                        </v-row>
+                        <v-divider/>
+                </v-col>
+                <v-col cols="10" sm="5" class="mt-6">
+                    <v-row>   
+                        <v-text-field
+                        outlined
+                        color="#a64e4b"
+                        placeholder="Email">
+                        </v-text-field>
+                    </v-row>
                         <v-btn
-                        class="ml-6 white--text"
+                        class="white--text"
                         color="#a64e4b"
                         large
                         rounded
-                        >
-                            Enviar    
+                        v-text="'Enviar'"
+                        >  
                         </v-btn>
-                        
-                    </v-col>    
-                    
-                </v-col>
-            </v-row>
-        </div>
+                </v-col> 
+            </v-col>
+        </v-row>
+    </div>
 </template>
 
 <script>
