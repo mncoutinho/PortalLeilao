924995819e092f1c6eb50855eb8d76238423c14a
Lucas Fernandes da Silva <Lucas_201130@hotmail.com>
2020-11-20 21:59:24 -0300

    componetizando e criando variaveis para padroes de botoes

---
+++ client/src/store/modules/layout/componentes/button.js
@@ -0,0 +1,24 @@
+export default{
+    state:{
+        //botoes formularios
+        buttonA:{
+            color: '#562B28',
+            type: "white--text",
+        },
+        //botoes bid's
+        buttonB:{
+            type: "white--text",
+            color: "#422321",
+        },
+        //botoes cartela
+        buttonC:{
+            type: "col-12 white--text",
+            color: "#422321"
+        },
+        //botoes home, ou para dá destaque
+        buttonD:{
+            type: "white--text",
+            color: "#a64e4b",
+        }
+    }
+}
